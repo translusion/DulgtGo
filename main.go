05f37292c9e2ef3fb0e3541425e8fc3a2d809285
Erik Engheim <erik.engheim@mac.com>
2014-05-08 00:24:39 +0200

    got scrypt working while letting user chose parameters

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
+// example usage: dulgt -password=foobar -len=12 amazon
+
 import (
 	"encoding/base64"
     "code.google.com/p/go.crypto/scrypt"
+    "flag"
     "fmt"
 )
 
-// recommended cost parameters for interactive login in 2009
-const N = 16384
-const r = 8
-const p = 1 
+var passwordStr = flag.String("password", "", "master password")
+var keyLen      = flag.Int("len", 8, "length of generated password")
 
-func main() {
-    keyLen := 8
-    password := []byte("some password")
-    salt := []byte("some salt")
-    dk, _ := scrypt.Key(password, salt, N, r, p, keyLen)
+// recommended cost parameters for interactive login in 2009
+var N           = flag.Int("N", 16384, "General cost.")
+var r           = flag.Int("r", 8, "Memory cost relative to total.")
+var p           = flag.Int("p", 1, "CPU cost relative to total.")
+
+func main() {    
+    flag.Parse()
+    password := []byte(*passwordStr)
+    salt := []byte(flag.Arg(0))         // use this as domain, or really hard password
+    
+    //to debug that we actually got a value for password
+    //fmt.Printf("password:%s, salt:%s, keylen: %d\n\n", *passwordStr, flag.Arg(0), *keyLen)
+    
+    dk, _ := scrypt.Key(password, salt, *N, *r, *p, *keyLen)
 	str := base64.StdEncoding.EncodeToString(dk)
     
     fmt.Println(str)
