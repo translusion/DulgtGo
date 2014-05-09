716a30026be46ea630fabb9eccef365ed878016e
Erik Engheim <erik.engheim@mac.com>
2014-05-09 16:46:31 +0200

    for debugging purposes

--- main.go
+++ main.go
@@ -5,27 +5,18 @@ package main
 import (
 	"encoding/base64"
     "code.google.com/p/go.crypto/scrypt"
-    "flag"
     "fmt"
 )
 
-var passwordStr = flag.String("password", "", "master password")
-var keyLen      = flag.Int("len", 8, "length of generated password")
+const keyLen = 8
 
 // recommended cost parameters for interactive login in 2009
-var N           = flag.Int("N", 16384, "General cost.")
-var r           = flag.Int("r", 8, "Memory cost relative to total.")
-var p           = flag.Int("p", 1, "CPU cost relative to total.")
+const N = 16384
+const r = 8
+const p = 1
 
 func main() {    
-    flag.Parse()
-    password := []byte(*passwordStr)
-    salt := []byte(flag.Arg(0))         // use this as domain, or really hard password
-    
-    //to debug that we actually got a value for password
-    //fmt.Printf("password:%s, salt:%s, keylen: %d\n\n", *passwordStr, flag.Arg(0), *keyLen)
-    
-    dk, _ := scrypt.Key(password, salt, *N, *r, *p, *keyLen)
+    dk, _ := scrypt.Key([]byte("mypassword"), []byte("mysalt"), N, r, p, keyLen)
 	str := base64.StdEncoding.EncodeToString(dk)
     
     fmt.Println(str)
